ad460f402c7e924b45799c5ba48743f6b65d99a0
Harender bhati <harrythakur1101@gmail.com>
2021-08-27 15:12:07 +0530

    reactJs

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Home</h2>
+            </div>
+        )
+    }
+}
+
+export default home
---
+++ src/components/nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class nav extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Hello Guys</h2>
+            </div>
+        )
+    }
+}
+
+export default nav
